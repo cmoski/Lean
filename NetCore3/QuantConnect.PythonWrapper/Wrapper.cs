85e4ab16f069c80eb25d46643b328743d39231cf
Leonardo-Ferreira <index.xp@gmail.com>
2020-03-19 20:16:37 -0300

    Created a wrapper for Python Added nuget packages

---
+++ NetCore3/QuantConnect.PythonWrapper/Wrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Python.Runtime
+{
+    public class PyObject
+    {
+        public PyObject()
+        {
+            throw new NotImplementedException("Python is not yet supported");
+        }
+    }
+
+    public class Py
+    {
+        public static IDisposable GIL()
+        {
+            throw new NotImplementedException("Python is not yet supported");
+        }
+    }
+}
