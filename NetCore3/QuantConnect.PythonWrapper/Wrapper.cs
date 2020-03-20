bf4d736a3d909fb606fff5ab6ad2e44ed6b0e12a
Leonardo-Ferreira <index.xp@gmail.com>
2020-03-19 23:54:24 -0300

    More Wrapping around pythons dependencies

--- NetCore3/QuantConnect.PythonWrapper/Wrapper.cs
+++ NetCore3/QuantConnect.PythonWrapper/Wrapper.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Python.Runtime
 {
-    public class PyObject
+    public class PyObject : List<PyObject>, IDisposable
     {
+        public IntPtr Handle { get; set; }
+
         public PyObject()
         {
             throw new NotImplementedException("Python is not yet supported");
+        }
+
+        public T As<T>()
+        {
+            throw new NotImplementedException("Python is not yet supported");
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetAttr(string attrName)
+        {
+            throw new NotImplementedException("Python is not yet supported");
+        }
+
+        public dynamic HasAttr(string attrName)
+        {
+            throw new NotImplementedException("Python is not yet supported");
+        }
+
+        public dynamic InvokeMethod(string methodName)
+        {
+            throw new NotImplementedException("Python is not yet supported");
+        }
+
+        public dynamic GetPythonType()
+        {
+            throw new NotImplementedException("Python is not yet supported");
+        }
+
+        public dynamic AsManagedObject(dynamic type)
+        {
+            throw new NotImplementedException("Python is not yet supported");
+        }
+
+        public dynamic Repr()
+        {
+            throw new NotImplementedException("Python is not yet supported");
+        }
+
+        public static implicit operator PyObject(PyList v)
+        {
+            throw new NotImplementedException();
         }
     }
 
@@ -15,6 +65,76 @@ namespace Python.Runtime
         public static IDisposable GIL()
         {
             throw new NotImplementedException("Python is not yet supported");
+        }
+
+        public static object Import(string import)
+        {
+            throw new NotImplementedException("Python is not yet supported");
+        }
+    }
+
+    public class PythonEngine
+    {
+        public static PyObject ModuleFromString(params string[] arguments)
+        {
+            throw new NotImplementedException("Python is not yet supported");
+        }
+
+        public static void Exec(string code, object p, object handle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PyList : IDisposable
+    {
+        public PyList(dynamic args)
+        {
+            throw new NotImplementedException("Python is not yet supported");
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Length()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsListType(PyObject pyObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PyDict : IDisposable
+    {
+        public object Handle { get; set; }
+
+        public PyDict(dynamic args = null)
+        {
+
+        }
+
+        public List<PyObject> Items()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetItem(string s, dynamic obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic GetItem(string item)
+        {
+            throw new NotImplementedException();
         }
     }
 
@@ -43,5 +163,31 @@ namespace Python.Runtime
         public PythonException(string message, System.Exception inner) : base(message, inner)
         {
         }
+    }
+
+    public static class PythonExt
+    {
+        public static dynamic ToPython(this Type T)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static dynamic DefineDynamicAssembly(this AppDomain domain, AssemblyName name, AssemblyBuilderAccess an)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PyString
+    {
+        public static bool IsStringType(PyObject item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public enum AssemblyBuilderAccess
+    {
+        Run = 1
     }
 }
